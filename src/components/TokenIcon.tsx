45400abd83f7be0a4e17018ede2877a195ffd993
huanyeeth <huanyeweb3@outlook.com>
2025-04-27 00:15:23 +0800

    fix: ipfs logo show issue

--- src/components/TokenIcon.tsx
+++ src/components/TokenIcon.tsx
@@ -1,9 +1,13 @@
 import BaseChainIcon from "../assets/base.svg";
 
 const TokenIcon = ({ logoURI }: { logoURI: string }) => {
+  const isIPFS = logoURI.startsWith("Qm");
+  const imageUrl = isIPFS
+    ? `https://gateway.pinata.cloud/ipfs/${logoURI}`
+    : logoURI;
   return (
     <div className="relative">
-      <img src={logoURI} className="w-6 h-6 rounded-full" alt="token" />
+      <img src={imageUrl} className="w-6 h-6 rounded-full" alt="token" />
       <img
         src={BaseChainIcon}
         className="w-2 h-2 absolute bottom-0 right-0"
