5e9c21d4986a382ceb48b6801163ade530497428
huanyeeth <huanyeweb3@outlook.com>
2025-05-05 16:46:44 +0800

    #fix update api

--- vite.config.ts
+++ vite.config.ts
@@ -32,7 +32,7 @@ export default defineConfig({
         rewrite: (path) => path.replace(/^\/beta-api/, '')
       },
       '/studio-api': {
-        target: 'http://127.0.0.1:5555',
+        target: 'https://api.nyko.cool',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/studio-api/, '')
       }
